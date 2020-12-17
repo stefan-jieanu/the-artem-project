ec9899f7ac50229d81a54e86255435a759080ffb
Sliwer <stefanalexjieanu@gmail.com>
2020-12-17 22:49:25 +0200

    Basic logging for OpenGL errors

---
+++ RealMVPArtem/src/logging/log_gl.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <GL/glew.h>
+
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    // Clear the error 'list' from opengl
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" << function <<
+            " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
